be73bec4ca427b6452dc36ff917a4cef254683ca
spideystreet <dhicham.pro@gmail.com>
2025-06-24 20:12:14 +0200

    feat: data loader sevice to connect db & fetch projects

---
+++ src/application/services/project_data_loader.py
@@ -0,0 +1,32 @@
+from typing import List
+from sqlalchemy.orm import Session
+from src.domain.models.schema import Project
+from src.infrastructure.postgres.database import SessionLocal
+
+
+class ProjectDataLoadingService:
+    """
+    A service responsible for loading project data from the database.
+    """
+
+    def __init__(self, db_session: Session = SessionLocal()):
+        """
+        Initializes the service with a database session.
+
+        Args:
+            db_session (Session): The SQLAlchemy session to use for database operations.
+        """
+        self.db_session = db_session
+
+    def get_all_projects(self) -> List[Project]:
+        """
+        Fetches all project records from the database.
+
+        Returns:
+            List[Project]: A list of all Project objects.
+        """
+        try:
+            projects = self.db_session.query(Project).all()
+            return projects
+        finally:
+            self.db_session.close() 
