0ed28bcdf2bdb779d952199f2e1beabb1f6ae34b
spideystreet <dhicham.pro@gmail.com>
2025-10-12 15:05:07 +0200

    feat: add trending=true for mapping

--- src/dagster/config/github_mapping.py
+++ src/dagster/config/github_mapping.py
@@ -1,20 +1,18 @@
 
-# Example mapping between GitHub scraper fields and the Project model from Prisma schema
-# You can adapt the keys/values according to your needs and the structure of the scraper data.
+# Mapping between GitHub scraper fields and the Project model from Prisma schema
+# Adapt the keys/values according to your needs and the structure of the scraper data.
 
 GITHUB_TO_PROJECT_MAPPING = {
 	# Project model key : source in the scraper dict
 	"title": "name",  # GitHub repo name
 	"description": "description",  # Repo description
-	"repoUrl": "html_url",  # GitHub repo URL
-	"provider": lambda _: "GITHUB",  # Fixed value
+	"repoUrl": "url",  # GitHub repo URL
 	"githubUrl": "html_url",  # GitHub URL
 	"websiteUrl": "homepage",  # Repo homepage
 	"published": lambda repo: True,  # Always published (example)
-	"owner": lambda repo: repo.get("owner", {}).get("login"),  # Owner login
+    "trending": lambda repo: True,  # Default to True, set True because is trending projects
 	"logoUrl": "owner.avatar_url",  # Owner avatar
 	"imagesUrls": lambda repo: [],  # Fill as needed
 	"createdAt": "created_at",  # Creation date
 	"updatedAt": "updated_at",  # Update date
-	# Add other fields if needed
 }
