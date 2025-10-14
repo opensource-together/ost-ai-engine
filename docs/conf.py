2674cd0a071ce91800c747a347934d8266ad0061
spideyai-X <dhicham.pro@gmail.com>
2025-10-14 11:00:40 +0200

    docs: furo theme

--- docs/conf.py
+++ docs/conf.py
@@ -6,4 +6,12 @@ release = '0.1.0'
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-html_theme = 'alabaster'
+
+# Furo theme
+html_theme = 'furo'
+html_theme_options = {
+    "sidebar_hide_name": False,
+    "navigation_with_keys": True,
+}
+
+html_static_path = ['_static']
