1f617d6480637cebceae31adcbe76ef48500525f
spideystreet <dhicham.pro@gmail.com>
2025-10-20 15:44:10 +0200

    fix: githubLogin -> githubUsername map

--- src/dagster/config/github_mapping.py
+++ src/dagster/config/github_mapping.py
@@ -3,7 +3,7 @@
 # Adapt the keys/values according to your needs and the structure of the scraper data.
 
 GITHUB_TO_PROJECT_MAPPING = {
-	"githubLogin": "owner.login",       # Github name of owner
+	"githubUsername": "owner.login",    # Github name of owner
 	"title": "name",                    # GitHub repo name
 	"description": "description",       # Repo description
 	"repoUrl": "html_url",  			# GitHub URL (repoUrl = html_url)
