4816f729d45fcde11fafa34291c237a67f91a324
spideystreet <dhicham.pro@gmail.com>
2025-10-20 13:51:07 +0200

    build: add githubLogin field + migration

--- src/dagster/config/github_mapping.py
+++ src/dagster/config/github_mapping.py
@@ -3,13 +3,13 @@
 # Adapt the keys/values according to your needs and the structure of the scraper data.
 
 GITHUB_TO_PROJECT_MAPPING = {
-	"ownerLogin": "owner.login",        # Owner login
+	"githubLogin": "owner.login",       # Github name of owner
 	"title": "name",                    # GitHub repo name
 	"description": "description",       # Repo description
 	"repoUrl": "html_url",  			# GitHub URL (repoUrl = html_url)
 	"provider": lambda repo: "GITHUB",  # Provider type
 	"githubUrl": "html_url",  			# GitHub URL
-	"gitlabUrl": None,  				# Not available from GitHub scraper
+	"gitlabUrl":  None,  				# Not available from GitHub scraper
 	"twitterUrl": None,  				# Not available from GitHub scraper
 	"linkedinUrl": None,  				# Not available from GitHub scraper
 	"discordUrl": None,  				# Not available from GitHub scraper
