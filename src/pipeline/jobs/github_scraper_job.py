ef2e338d339265e896b3199437c019b3e5fd763d
spideystreet <dhicham.pro@gmail.com>
2025-11-24 13:48:56 +0100

    refactor: using in process executor to avoid SIGBUS errors with python

--- src/pipeline/jobs/github_scraper_job.py
+++ src/pipeline/jobs/github_scraper_job.py
@@ -1,6 +1,7 @@
 from dagster import (
     define_asset_job,
     in_process_executor,
+    multiprocess_executor,
     AssetSelection,
     RetryPolicy,
     Backoff,
@@ -11,9 +12,8 @@ from dagster import (
 github_scraper_job = define_asset_job(
     name="github_scraper_job",
     selection=AssetSelection.groups("github_projects_scraper"),
-    executor_def=in_process_executor,
-    # default retry policy for ops computing assets in this job.
-    op_retry_policy=RetryPolicy(
+    executor_def=in_process_executor,  # Avoid SIGBUS with multiprocessing
+    op_retry_policy=RetryPolicy( # default retry policy for ops computing assets in this job.
         max_retries=2,
         delay=30,
         backoff=Backoff.EXPONENTIAL,
