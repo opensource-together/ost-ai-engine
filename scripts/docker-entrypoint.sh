1b859024d54176d454a72dd8875d13385f761132
spideystreet <dhicham.pro@gmail.com>
2025-12-04 20:01:14 +0100

    refactor: Makefile content on entrypoint for auto-deployment

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -3,6 +3,16 @@ set -e
 
 python ./config/cfg.py
 
+echo "Starting database setup..."
+
+echo "Running Prisma migrations..."
+npx prisma migrate dev
+echo "Prisma migrations completed."
+
+echo "Running database seeding..."
+npx ts-node --compiler-options '{"module":"commonjs"}' prisma/seed/seed.ts
+echo "Database seeding completed."
+
 python ./scripts/cfg_cron.py &
 
 cmd="$1"
