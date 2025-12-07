df5296244d896db0ed5984606a27d8013c5b4b9a
spideystreet <dhicham.pro@gmail.com>
2025-12-07 16:37:07 +0100

    build: entrypoint fix to running migrations

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -3,15 +3,18 @@ set -e
 
 python ./config/cfg.py
 
-echo "Starting database setup..."
+if [ "$RUN_MIGRATIONS" = "true" ]; then
+  echo "Starting database setup..."
+  echo "Running Prisma migrations..."
+  npx prisma migrate deploy
+  echo "Prisma migrations completed."
 
-echo "Running Prisma migrations..."
-npx prisma migrate dev
-echo "Prisma migrations completed."
-
-echo "Running database seeding..."
-npx ts-node --compiler-options '{"module":"commonjs"}' prisma/seed/seed.ts
-echo "Database seeding completed."
+  echo "Running database seeding..."
+  npx ts-node --compiler-options '{"module":"commonjs"}' prisma/seed/seed.ts
+  echo "Database seeding completed."
+else
+  echo "Skipping database setup (RUN_MIGRATIONS not set to true)."
+fi
 
 python ./scripts/cfg_cron.py &
 
